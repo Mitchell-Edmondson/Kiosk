95e8e44f967e720bb2af4abaff4d55049d7943a0
Jordan-Castillo <jtcastil@calpoly.edu>
2017-10-25 14:32:30 -0700

    Additions to Layout.html and popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
+		setter();
       document.querySelector('button').addEventListener('click', main);      
+      document.querySelector('button').addEventListener('click', makePrice);      
 });
 function main() {
     var source = document.getElementById('source').value;
     document.getElementById("result").innerHTML = source;
+}
+function makePrice() {
+	var price, quantity, total;
+	
+	price = 5;
+	quantity = 14;
+	total = price * quantity;
+	
+	var el = document.getElementById('cost');
+	el.textContent = '$' + total;
+}
+function setter() {
+	var bar = document.getElementById('autoSet');
+	bar.textContent = 'Should appear at startup.';
 }
